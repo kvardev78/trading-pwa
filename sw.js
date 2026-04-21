8fdf193bea3ddcf5b7e60263347c5ab8b5bfcb85
kvardev78 <274788804+kvardev78@users.noreply.github.com>
2026-04-21 10:04:41 +0300

    update sw

--- sw.js
+++ sw.js
@@ -1,46 +1,11 @@
-const CACHE_NAME = "trading-app-cache-v1";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./main.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-// Install
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-// Activate
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
+self.addEventListener("activate", () => {
+  clients.claim();
 });
 
-// Fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
